f2dee8b546ed6c557453da7c9d9e0e51fdedda46
Pranay Sharma <121673814+impranaysharma@users.noreply.github.com>
2024-04-15 14:55:59 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -2,14 +2,17 @@ import './App.css'
 import Navbar from './navbar.jsx'
 import Form from './form.jsx'
 import About from './about.jsx'
-import {
+import
+  {
   BrowserRouter as Router,
   Route
 } from "react-router-dom";
+
 export default function App() {
+  
   return (
     <main>
-      <Navbar heading="Text Utlis"/>
+      <Navbar heading="Text-Utlis"/>
       <alert/>
         <div>      
             <div id='form'> <Form/></div>        
